a7dbf4ad2a3846e0089a368e705ce90683a75ac7
smartbitsapps <gapparena@gmail.com>
2016-07-12 17:35:17 +0000

    adds style for devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
+         
+  # Devise validates email and password automatically
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  
+  enum role: [:pending, :user, :manager, :admin]
+  enum status: [:banned, :inactive, :active]
+  after_initialize :set_default_role_and_status, :if => :new_record?
+
+  def set_default_role_and_status
+    self.role ||= :pending
+    self.status ||= :inactive
+  end
+  
 end

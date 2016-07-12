3970f86fce9588859e91fda504da3ec19bb78321
smartbitsapps <gapparena@gmail.com>
2016-07-12 16:03:19 +0000

    adds little language changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    domain: 'heroku.com',
+    #domain: 'heroku.com',
     address: 'smtp.gmail.com',
     port: 587,
     authentication: :plain,
